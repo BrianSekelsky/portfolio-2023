0f3f1f19cc37106355c8be140c0babbbb7b31fe3
Brian Sekelsky <bsekels@ncsu.edu>
2023-09-17 18:09:40 -0400

    basic structure

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		fontFamily: {
+			sans: ['"NeueMontrealMono"', 'sans-serif']
+		},
+		colors: {
+			'blue': '#0000ff',
+			'black': '#000',
+			'white': '#fff'
+		}
 	},
 	plugins: [],
 }
