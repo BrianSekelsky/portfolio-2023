0f7e2e3663995372d1f6715423cfcb1a5d21a41b
Brian Sekelsky <bsekels@ncsu.edu>
2023-09-18 07:11:56 -0400

    style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,8 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		fontFamily: {
-			sans: ['"NeueMontrealMono"', 'sans-serif']
+			sans: ['"NeueMontrealMono"', 'sans-serif'],
+			serif: ['"EditorialNew"', 'serif']
 		},
 		colors: {
 			'blue': '#0000ff',
