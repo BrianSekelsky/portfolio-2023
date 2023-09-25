339d399674f8f4a3f29ce540599b512a4816cfc8
Brian Sekelsky <bsekels@ncsu.edu>
2023-09-25 07:42:28 -0400

    Image component test

---
+++ .netlify/functions-internal/chunks/music_4abeeab1.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/music_2554d0b3.mjs');
+
+export { page };
--- .netlify/functions-internal/entry.mjs
+++ .netlify/functions-internal/entry.mjs
@@ -1,6 +1,6 @@
 import * as adapter from '@astrojs/netlify/netlify-functions.js';
 import { renderers } from './renderers.mjs';
-import { manifest } from './manifest_8f42dcdc.mjs';
+import { manifest } from './manifest_64a94529.mjs';
 import 'cookie';
 import 'kleur/colors';
 import 'string-width';
@@ -11,12 +11,12 @@ import './chunks/astro_96538662.mjs';
 import 'mime';
 import 'path-to-regexp';
 
-const _page0  = () => import('./chunks/image-endpoint_bfc92d85.mjs');
-const _page1  = () => import('./chunks/index_aa61f777.mjs');
+const _page0  = () => import('./chunks/image-endpoint_87299d6d.mjs');
+const _page1  = () => import('./chunks/index_f907c92c.mjs');
 const _page2  = () => import('./chunks/project-1_ae683983.mjs');
 const _page3  = () => import('./chunks/project-2_39eba426.mjs');
 const _page4  = () => import('./chunks/about_43cb3c79.mjs');
-const _page5  = () => import('./chunks/music_c7e6c613.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/image-endpoint.js", _page0],["src/pages/index.astro", _page1],["src/pages/projects/project-1.astro", _page2],["src/pages/projects/project-2.astro", _page3],["src/pages/about.astro", _page4],["src/pages/music.astro", _page5]]);
+const _page5  = () => import('./chunks/music_9c142961.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/image-endpoint.js", _page0],["src/pages/index.astro", _page1],["src/pages/projects/project-1.astro", _page2],["src/pages/projects/project-2.astro", _page3],["src/pages/about.astro", _page4],["src/pages/music.astro", _page5]]);
 const _manifest = Object.assign(manifest, {
 	pageMap,
 	renderers,
