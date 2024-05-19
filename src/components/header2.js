11cdba9236637188848fcd75eaa9cb25bc846810
Brian Sekelsky <bsekels@ncsu.edu>
2024-05-19 13:34:58 -0400

    p5

---
+++ src/components/header2.js
@@ -0,0 +1,5 @@
+import p5 from "p5";
+
+const mySketch = "/sketch.js";
+
+new p5(mySketch, document.getElementById("header"));
---
+++ src/components/sketch.js
@@ -0,0 +1,7 @@
+function setup() {
+    canvas = createCanvas(600, 600);
+}
+
+function draw() {
+    background(255, 0, 0);
+}
