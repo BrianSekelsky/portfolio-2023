d53752e4b68299b45d3665a321c588f6799a5952
Brian Sekelsky <brianmini@Brians-Mac-mini.local>
2025-07-06 11:18:42 -0400

    change up cards

--- src/components/card-map.js
+++ src/components/card-map.js
@@ -13,7 +13,6 @@ import ScaleWorlds from "./Gallery-cards/ScaleWorlds.astro";
 import DeliberateAI from "./Gallery-cards/DeliberateAI.astro";
 import TerraFirma from "./Gallery-cards/TerraFirma.astro";
 import PossibleInternet from "./Gallery-cards/PossibleInternet.astro";
-import LAS from "./Gallery-cards/LAS.astro";
 import NPCA from "./Gallery-cards/NPCA.astro";
 
 
@@ -21,15 +20,13 @@ export default [
   { Component: SizeAndScale, typeClass: "branding website ux caseStudy" },
   { Component: Fidelity, typeClass: "website ux research caseStudy" },
   { Component: CriticalImages, typeClass: "branding" },
-  { Component: DeliberateAI, typeClass: "website ux" },
   { Component: ImaginePublicTransit, typeClass: "branding" },
   { Component: WitnessingGlaciers, typeClass: "website branding caseStudy" },
   { Component: PossibleInternet, typeClass: "userExperience caseStudy" },
   { Component: Vespertine, typeClass: "branding website branding" },
   { Component: TerraFirma, typeClass: "branding website branding" },
   { Component: ScaleWorlds, typeClass: "userExperience caseStudy" },
-  { Component: LAS, typeClass: "userExperience caseStudy" },
-  { Component: Nothing, typeClass: "userExperience" },
   { Component: NPCA, typeClass: "website" },
+  { Component: Nothing, typeClass: "userExperience" },
   { Component: DeliberateAI, typeClass: "website userExperience" },
 ];
