e03532e12011577dbf42c0f02cb402397fffb5d3
Brian Sekelsky <bsekels@ncsu.edu>
2026-06-18 10:37:25 -0500

    Style overhaul

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
   output: "static",
   // adapter: netlify()
 
@@ -23,6 +22,7 @@ export default defineConfig({
 
   // Vite optimizations for code splitting
   vite: {
+    plugins: [tailwindcss()],
     build: {
       cssCodeSplit: true,
       rollupOptions: {
